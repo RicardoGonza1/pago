6383d90b31fc36ee1ecf483cf7061a79d06734a9
RicardoGonza1 <76696984+RicardoGonza1@users.noreply.github.com>
2021-05-13 14:18:30 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,8 @@
+addEventListener('DOMContentLoaded', () =>{
+	const btn_menu = document.querySelector('.btn_menu')
+	if(btn_menu){
+		btn_menu.addEventListener('click', () => {
+			const menu_items.classList.toogle('show')
+		})
+	}
+})
