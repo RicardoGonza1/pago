46248412b7f3cd342dd4cef8f3677fc17c03e4d1
RicardoGonza1 <76696984+RicardoGonza1@users.noreply.github.com>
2021-05-14 12:05:47 -0500

    Add files via upload

---
+++ 01.php
@@ -0,0 +1,161 @@
+<?php/*
+
+    session_start();
+
+    if(!isset($_SESSION['rol'])){
+        header('location: login.php');
+    }else{
+        if($_SESSION['rol'] != 2){
+            header('location: login.php');
+        }
+    }
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+</head>
+<body>
+   <!DOCTYPE html>
+<html>
+
+<head>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Anyela Liseth Ome</title>
+   <link rel="stylesheet" href="style.css"><!--
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">-->
+    <link rel="shortcut icon" type="image/png" href="mujer.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+
+<style type="text/css">
+    .boton{
+    padding: 10px;
+    color: #fff;
+    background: #0098cb;
+    width: 200px;
+    margin: 20px auto;
+    margin-top: 0;
+    border: 3px;
+    border-radius: 3px;
+    cursor: pointer;
+}
+ .cerrarc{
+    padding: 10px;
+    color: #fff;
+    background: #0098cb;
+    width: 100px;
+    margin: 20px auto;
+    margin-top: 0;
+    border: 3px;
+    border-radius: 3px;
+    cursor: pointer;
+
+}
+.cerrarcesion{
+    text-align: center;
+  
+}
+</style>
+
+
+
+
+            
+            
+        </div>
+        
+<body>
+   <script>/*
+var getin = prompt("Ingresa tu numero de cedula para entrar a tu cuenta")
+if (getin!="1083891603")
+{location.href='acceso_denegado.html'}
+else
+{}*/
+</script>
+    
+<div id="page" class="wrapper">
+        <div class="container-fluid">
+            <div class="row text-center">
+              <div class="col-12">
+
+<h4 class="cerrarcesion">
+                    <button type="button" class="cerrarc" onclick="window.open('cerrar.php')">Cerrar cesion</button>
+                </h4>
+                <h1 id="nameweb" class="name-web">FastRed</h1>
+                              </div>
+              <h2>Bienvenida señora Anyela Liseth Ome</h2>
+              
+              <h6>
+
+                <button type="button" class="boton" onclick="window.open('http://192.168.1.1')">Administra tu router</button>
+                
+                <br>
+                 <h4><a href="metodos_de_pago.html">Paga tu factura</a></h4>
+              </h6>
+              <h2>Tu cuenta esta paga hasta el 1/06/2021 <br>
+              aqui esta tu historial de facturas</h2>
+              <h6>
+              <a href="01-Anyela-Liseth-Ome.pdf" target="_blank">Factura 1/04/2021 a 1/05/2021</a><br>
+              <br>  
+              <a href="04-Anyela-Liseth-Ome.pdf" target="_blank">Factura 1/05/2021 a 1/06/2021</a>
+            </h6>
+            <h2>Tu siguiente factura</h2>
+            
+              
+            
+              <br>
+              <p> <a href="contador1.html" target="_blank">Factura 1/06/2021 a 1/07/2021</a></p>
+            
+
+                <div class="col-10">
+                  <div class="card">
+                      
+                </div>
+              </div>
+            </div>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://wa.me/573142425831" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+       
+<script type="text/javascript">
+        const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  btnSwitch.classList.toggle('active');
+
+  // Guardamos el modo en localstorage.
+  if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
+  }
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+  document.body.classList.add('dark');
+  btnSwitch.classList.add('active');
+} else {
+  document.body.classList.remove('dark');
+  btnSwitch.classList.remove('active');
+}
+      </script>
+<!-- Start of starcomunicaciones Zendesk Widget script -->
+<!--<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=d2531f35-efb2-48f5-9b8b-ed3a22e40fbe"> </script>-->
+<!-- End of starcomunicaciones Zendesk Widget script -->
+
+</body>
+</html>
+</body>
+</html>
